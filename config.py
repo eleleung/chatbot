200e621790edc6e18daaf97985d10a37a0b3ba97
eleleung <eleanor.sl.leung@gmail.com>
2017-10-15 14:41:10 +0800

    chatbot model and data utils added

---
+++ config.py
@@ -0,0 +1,35 @@
+"""
+Config parameters and hyperparameters
+"""
+
+# cornell movie-dialogs data paths
+DATA_PATH = '/Users/EleanorLeung/Documents/CITS4404/chatbot/data/cornell movie subset'
+CONVO_FILE = 'movie_conversations.txt'
+LINE_FILE = 'movie_lines.txt'
+OUTPUT_FILE = 'output_convo.txt'
+PROCESSED_PATH = 'processed_data'
+
+# min. num of times a word must appear to be added to vocab
+THRESHOLD = 1
+
+# reserved vocab tokens
+PAD_ID = 0
+UNK_ID = 1
+START_ID = 2
+EOS_ID = 3
+
+# percentage split of data between training and testing
+TEST_SET_PERCENTAGE = 0
+
+# bucket sizes
+BUCKETS = [(8, 10), (12, 14), (16, 19)]
+
+# model parameters
+NUM_LAYERS = 3
+HIDDEN_SIZE = 256
+BATCH_SIZE = 64
+
+LR = 0.5
+MAX_GRAD_NORM = 5.0
+
+NUM_SAMPLES = 512
---
+++ phrase_pair_dao.py
@@ -0,0 +1,33 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/chatbot")
+db = client['chatbot']
+
+class PhrasePair:
+    def __init__(self, name=None, x=None, y=None, dictionary=None):
+        self.dict = dict({
+            'name': name,
+            'x': x,
+            'y': y
+        })
+        if dictionary is not None:
+            self.dict = dictionary
+
+def save_data(phrase_pairs):
+    dicts = [phrase_pair.dict for phrase_pair in phrase_pairs]
+
+    count = 0
+    for dict in dicts:
+        db.phrase_pairs.insert_one(dict, True)
+        count += 1
+
+    print(count)
+
+def load_data(ids=None):
+    collection = db.phrase_pairs
+
+    if ids is None:
+        phrase_pairs_dict = collection.find()
+
+    phrase_pairs = [PhrasePair(dictionary=dict) for dict in phrase_pairs_dict]
+    return sorted(phrase_pairs, key=lambda phrase_pair: phrase_pair.dict['_id'])
